bb909b1c7787007141175ddb2eacb902d9ad302a
sanavinaik10 <67157233+sanavinaik10@users.noreply.github.com>
2020-09-15 20:33:34 +0530

    Add files via upload

--- kitchen.js
+++ kitchen.js
@@ -1,4 +1,5 @@
 img = "";
+objects = [];
 function preload(){
     img = loadImage("kitchen.jpg");
 }
@@ -14,6 +15,18 @@ function modelLoaded(){
     objectDetector.detect(img, gotResult);
 }
 function draw(){
+    image(img, 0, 0, 600,420);
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            fill("#FF0000");
+            percentage = floor(objects[i].confidence * 100);
+            text(objects[i].label+ " " + percentage + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 function gotResult(error,result){
     if(error){
