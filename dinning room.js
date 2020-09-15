293badc82974e005080b241f57121f4bd8a666f2
sanavinaik10 <67157233+sanavinaik10@users.noreply.github.com>
2020-09-15 19:23:11 +0530

    Add files via upload

---
+++ dinning room.js
@@ -0,0 +1,26 @@
+img = "";
+function preload(){
+    img = loadImage("dinning_room.jpg");
+}
+function setup(){
+    canvas = createCanvas(600,420);
+    canvas.position(450,190);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: In Detection Mode";
+}
+function modelLoaded(){
+    console.log("Model is loaded!!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function draw(){
+}
+function gotResult(error,result){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(result);
+        objects = result;
+    }
+}
